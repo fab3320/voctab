45012263c04d5cd5970e02ad663fff493e84b70c
Fab3320 <fab3320@gmail.com>
2024-10-19 15:56:53 +0200

    simplification du code / random list flashcard

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect, useCallback} from 'react';
+import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
 import {extract_flashcard} from './utils/extractvoc2.js';
-import { useAssets,Asset } from 'expo-asset';
+import {useAssets, Asset} from 'expo-asset';
+import _ from "lodash";
 
 
-function DisplayFlashcard({ langue1, langue2, flashcard, onNextQuestion, otherLangue, displayedLangue,forceLangue, setForceLangue}){
+function DisplayFlashcard({
+                              flashcard,
+                              nextFlashcard,
+                              otherLangue,
+                              displayedLangue,
+                          }) {
     const [traduction, setTraduction] = useState('');
     const [isCorrect, setIsCorrect] = useState(false);
     const [compteur, setCompteur] = useState(0);
     const [skipflashcard, setSkipFlashcard] = useState('');
+
+    useEffect(() => {
+        setTraduction(''); // On réinitialise la traduction
+        setIsCorrect(false); // On réinitialise le statut de la réponse
+    }, [flashcard, displayedLangue, otherLangue]);
 
     const verifyAnswer = () => {
         console.log("Traduction entrée:", traduction);
@@ -18,7 +29,6 @@ function DisplayFlashcard({ langue1, langue2, flashcard, onNextQuestion, otherLa
         const correct = traduction === flashcard[otherLangue]
         if (correct) {
             setCompteur(compteur + 1);
-            onNextQuestion(setIsCorrect, setTraduction);
         }
         console.log("Résultat de la vérification:", traduction === flashcard[otherLangue]);
     };
@@ -29,105 +39,55 @@ function DisplayFlashcard({ langue1, langue2, flashcard, onNextQuestion, otherLa
             <Text>{displayedLangue} : {flashcard[displayedLangue]}</Text>
             <Text>{otherLangue} : "??__??"</Text>
 
-            <Text>"la réponse est : {flashcard[otherLangue]}"</Text>
+            <Text style={{fontStyle: "italic", fontWeight: "100"}}>(temporaire: "la réponse est
+                : {flashcard[otherLangue]}")</Text>
             <TextInput
                 placeholder={`Entrez la traduction en ${otherLangue}`}
                 value={traduction}
                 onChangeText={setTraduction}
                 style={styles.inputStyle}
             />
+
+            {isCorrect && <Text>Bravo !</Text>}
+
             <Button
                 title={isCorrect ? "Question suivante" : "Vérifier"}
-                onPress={isCorrect ? () => onNextQuestion(setIsCorrect, setTraduction) : verifyAnswer}
+                onPress={isCorrect ? nextFlashcard : verifyAnswer}
             />
 
             <Button
                 title="Passer"
                 onPress={() => {
                     setSkipFlashcard(`réponse du dernier mot passé ==> ${flashcard[displayedLangue]} : ${flashcard[otherLangue]}`);
-                    onNextQuestion(setIsCorrect, setTraduction); // On passe à la question suivante
+                    nextFlashcard(); // On passe à la question suivante
                 }}
 
             />
-            <Button
-                title={
-                    forceLangue === '0'
-                        ? `testé en aléatoire`
-                        : forceLangue === '1'
-                            ? `testé en ${langue2} => ${langue1}`
-                            : `testé en ${langue1} => ${langue2}`
-                }
-                onPress={() => {
-                    // Alterner entre les trois états : '0', '1', et '2'
-                    const newForceLangue =
-                        forceLangue === '0' ? '1' : forceLangue === '1' ? '2' : '0';
-                    setForceLangue(newForceLangue);
-                    onNextQuestion(setIsCorrect, setTraduction); // Actualise la question avec la nouvelle valeur
-                }}
-            />
-            {isCorrect && <Text>Bravo !</Text>}
-
         </View>
     );
 
 }
 
+let headers = [];
+let flashcards = [];
 
 export default function App() {
-    const [headers, setHeaders] = useState([]); // Initialisation comme tableau vide
-    const [flashcards, setFlashcards] = useState([]);
     // Ligne définie initialement à null (mais on va la changer aléatoirement + loin)
-    const [currentFlashcard, setCurrentFlashcard] = useState(null);
+    const [currentFlashcard, setCurrentFlashcard] = useState(-1);
     //Ici,j'affiche la langue 1 par défaut
     const [displayedLangue, setDisplayedLangue] = useState('langue1');
     const [otherLangue, setOtherLangue] = useState('langue2')
-    const [forceLangue, setForceLangue] = useState('0');
-
-    // Ici, chaque flashcard possède la structure suivante :
-    // {"langue1": 'mot1', "langue2": 'mot2', "index" : int}
-    // Ci-dessous, 'useEffect' est également un Hook de React. Elle est exécutée après le rendu de la page.
-    // En l'occurence, elle est utilisée pour charger les en-têtes et les flashcards depuis le fichier Excel.
-    useEffect(() => {
-        const fetchHeaders = async () => {
-            try {
-                const asset = Asset.fromModule(require('./assets/base_voc/voc.xlsx'));
-                await asset.downloadAsync();
-                const result = await extract_flashcard(asset);
-                setHeaders(result.headers);
-                setFlashcards(result.flashcards);
-                if(result.flashcards.length === 0) { // Contrôlons qu'il y a bien des flashcards
-                    console.error("Aucune flashcard trouvée !");
-                    return;
-                }
-                const randomLineNumber = Math.floor(Math.random() * result.flashcards.length);
-                if (forceLangue === '0') {
-                const randomLangue = Math.random() <0.5 ? result.headers[0] : result.headers[1];
-                const otherLangue = randomLangue === result.headers[0] ? result.headers[1] : result.headers[0];
-                setDisplayedLangue(randomLangue); // Initialisation aléatoire de la langue affichée
-                setOtherLangue(otherLangue); // Initialisation de la langue de la traduction
-                } else if (forceLangue === '1') {
-                    setDisplayedLangue(result.headers[0]);
-                    setOtherLangue(result.headers[1]);
-                }
-                else if (forceLangue === '2') {
-                    setDisplayedLangue(result.headers[1]);
-                    setOtherLangue(result.headers[0]);
-                }
-
-                setCurrentFlashcard(randomLineNumber); // Initialisation après chargement
-            } catch (error) {
-                console.error("Erreur lors de l'extraction des en-têtes :", error);
-            }
-        };
-        fetchHeaders();
-    }, []);
+    const [forceLangue, setForceLangue] = useState('2');
 
 
-    const nextFlashcard = (setIsCorrectFunc, setTraduction) => {
-        setIsCorrectFunc(false); // On réinitialise le statut de la réponse
-        setTraduction(''); // On réinitialise le texte de "consigne"
-        if (forceLangue === '0') {
-            const randomLangue = Math.random() <0.5 ? headers[0] : headers[1];
+    const randomizeFlashcardsOrder = () => {
+        flashcards = _.shuffle(flashcards);
+    }
+
+    const nextFlashcard = () => {
+        console.log('nextFlashcard', headers)
+        if (forceLangue === 'random') {
+            const randomLangue = Math.random() < 0.5 ? headers[0] : headers[1];
             const otherLangue = randomLangue === headers[0] ? headers[1] : headers[0];
             setDisplayedLangue(randomLangue); // On change la langue affichée
             setOtherLangue(otherLangue); // On change la langue de la traduction (autre langue que celle affichée
@@ -140,34 +100,83 @@ export default function App() {
         }
 
         // si on est ici, cest que les flashcards ont déja été chargées, flashcards.length > 0
-        const randomLineNumber = Math.floor(Math.random() * flashcards.length);
-        setCurrentFlashcard(randomLineNumber);
-    };
+        setCurrentFlashcard(currentFlashcard + 1);
+    }
 
+
+    // Ici, chaque flashcard possède la structure suivante :
+    // {"langue1": 'mot1', "langue2": 'mot2', "index" : int}
+    // Ci-dessous, 'useEffect' est également un Hook de React. Elle est exécutée après le rendu de la page.
+    // En l'occurence, elle est utilisée pour charger les en-têtes et les flashcards depuis le fichier Excel.
+    useEffect(() => {
+        const fetchHeaders = async () => {
+            try {
+                const asset = Asset.fromModule(require('./assets/base_voc/voc.xlsx'));
+                await asset.downloadAsync();
+                const result = await extract_flashcard(asset);
+                headers = result.headers;
+                flashcards = result.flashcards;
+                if (result.flashcards.length === 0) { // Contrôlons qu'il y a bien des flashcards
+                    console.error("Aucune flashcard trouvée !");
+                    return;
+                }
+
+                randomizeFlashcardsOrder()
+                nextFlashcard()
+
+            } catch (error) {
+                console.error("Erreur lors de l'extraction des en-têtes :", error);
+            }
+        };
+        fetchHeaders();
+    }, []);
 
 
     const flashcard = currentFlashcard !== null ? flashcards[currentFlashcard] : null;
 
     return (
         <View style={styles.container}>
-            {flashcard && headers.length > 1 && (
+            {flashcard && displayedLangue && (
                 <DisplayFlashcard
-                    langue1={headers[0]}
-                    langue2={headers[1]}
                     flashcard={flashcard}
-                    onNextQuestion={nextFlashcard}
-                    otherLangue={otherLangue}
+                    nextFlashcard={nextFlashcard}
                     displayedLangue={displayedLangue}
-                    forceLangue={forceLangue}
-                    setForceLangue={setForceLangue}
+                    otherLangue={otherLangue}
                 />
             )}
-            <StatusBar style="auto" />
+            <Button
+                title={
+                    forceLangue === 'random'
+                        ? `aléatoire`
+                        : forceLangue === '1'
+                            ? `${displayedLangue} => ${otherLangue}`
+                            : `${otherLangue} => ${displayedLangue}`
+                }
+                onPress={() => {
+                    // Alterner entre les trois états : 'random', '1', et '2'
+                    const newForceLangue =
+                        forceLangue === 'random' ? '1' : forceLangue === '1' ? '2' : 'random';
+                    setForceLangue(newForceLangue);
+                    nextFlashcard(); // Actualise la question avec la nouvelle valeur
+                }}
+            />
+            { currentFlashcard < flashcards.length ?
+                <Text>{currentFlashcard + 1} / {flashcards.length}</Text> :
+                <><Text>Fin des flashcards</Text>
+                    <Button
+                        title="Recommencer"
+                        onPress={() => {
+                            randomizeFlashcardsOrder()
+                            setCurrentFlashcard(0)
+                        }}
+                    />
+                </>
+            }
+
+            <StatusBar style="auto"/>
         </View>
     );
 }
-
-
 
 
 const styles = StyleSheet.create({
--- TODO.md
+++ TODO.md
@@ -1,12 +1,15 @@
 
 TODO pour produit intermédaire
 
-# Aléatoiriser le sens de la traduction*
-# Indicateur (phrase) du sens de la traduction*
+# Aléatoiriser le sens de la traduction* ok
+
+# Indicateur (phrase) du sens de la traduction* ok 
+
 
 TODO (peut-être) pour le produit final
 
-# Empêcher d'avoir 2 fois la même question ? ==> créer une liste qui garde la trace de l'ID des questions déjà posées
+# Empêcher d'avoir 2 fois la même question ? ==> créer une liste qui garde la trace de l'ID des questions déjà posées==> ok
 
 # (?) I/O des fichiers excels
-# (?) Score minimaliste
+# (?) Score minimaliste ok
+
